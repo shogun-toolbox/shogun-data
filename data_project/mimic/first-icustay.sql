f7cc5a85c3b821e915e4b01912e97f238f428efb
Olivier <nguyenolive@gmail.com>
2017-08-29 17:35:14 -0400

    added lab events in sql query

--- data_project/mimic/first-icustay.sql
+++ data_project/mimic/first-icustay.sql
@@ -152,16 +152,32 @@ with ce_stg as
     -- here we assign labels to ITEMIDs
     -- this also fuses together multiple ITEMIDs containing the same data
     , case
-          when itemid = 50885 then 'BILIRUBIN'
-          when itemid = 50912 then 'CREATININE'
-          when itemid = 50809 then 'GLUCOSE'
-          when itemid = 50931 then 'GLUCOSE'
-          when itemid = 50811 then 'HEMOGLOBIN'
-          when itemid = 51222 then 'HEMOGLOBIN'
-          when itemid = 50824 then 'SODIUM'
-          when itemid = 50983 then 'SODIUM'
-          when itemid = 51300 then 'WBC'
-          when itemid = 51301 then 'WBC'
+        WHEN itemid = 50868 THEN 'ANION GAP'
+        WHEN itemid = 50862 THEN 'ALBUMIN'
+        WHEN itemid = 51144 THEN 'BANDS'
+        WHEN itemid = 50882 THEN 'BICARBONATE'
+        WHEN itemid = 50885 THEN 'BILIRUBIN'
+        WHEN itemid = 50912 THEN 'CREATININE'
+        WHEN itemid = 50806 THEN 'CHLORIDE'
+        WHEN itemid = 50902 THEN 'CHLORIDE'
+        WHEN itemid = 50809 THEN 'GLUCOSE'
+        WHEN itemid = 50931 THEN 'GLUCOSE'
+        WHEN itemid = 50810 THEN 'HEMATOCRIT'
+        WHEN itemid = 51221 THEN 'HEMATOCRIT'
+        WHEN itemid = 50811 THEN 'HEMOGLOBIN'
+        WHEN itemid = 51222 THEN 'HEMOGLOBIN'
+        WHEN itemid = 50813 THEN 'LACTATE'
+        WHEN itemid = 51265 THEN 'PLATELET'
+        WHEN itemid = 50822 THEN 'POTASSIUM'
+        WHEN itemid = 50971 THEN 'POTASSIUM'
+        WHEN itemid = 51275 THEN 'PTT'
+        WHEN itemid = 51237 THEN 'INR'
+        WHEN itemid = 51274 THEN 'PT'
+        WHEN itemid = 50824 THEN 'SODIUM'
+        WHEN itemid = 50983 THEN 'SODIUM'
+        WHEN itemid = 51006 THEN 'BUN'
+        WHEN itemid = 51300 THEN 'WBC'
+        WHEN itemid = 51301 THEN 'WBC'
         else null
       end as label
     , valuenum
@@ -174,16 +190,32 @@ with ce_stg as
       and lab.ITEMID in
       (
         -- comment is: LABEL | CATEGORY | FLUID | NUMBER OF ROWS IN LABEVENTS
-        50885, -- BILIRUBIN, TOTAL | CHEMISTRY | BLOOD | 238277
-        50912, -- CREATININE | CHEMISTRY | BLOOD | 797476
-        50931, -- GLUCOSE | CHEMISTRY | BLOOD | 748981
-        50809, -- GLUCOSE | BLOOD GAS | BLOOD | 196734
-        51222, -- HEMOGLOBIN | HEMATOLOGY | BLOOD | 752523
-        50811, -- HEMOGLOBIN | BLOOD GAS | BLOOD | 89712
-        50983, -- SODIUM | CHEMISTRY | BLOOD | 808489
-        50824, -- SODIUM, WHOLE BLOOD | BLOOD GAS | BLOOD | 71503
-        51301, -- WHITE BLOOD CELLS | HEMATOLOGY | BLOOD | 753301
-        51300  -- WBC COUNT | HEMATOLOGY | BLOOD | 2371
+          50868, -- ANION GAP | CHEMISTRY | BLOOD | 769895
+          50862, -- ALBUMIN | CHEMISTRY | BLOOD | 146697
+          51144, -- BANDS - hematology
+          50882, -- BICARBONATE | CHEMISTRY | BLOOD | 780733
+          50885, -- BILIRUBIN, TOTAL | CHEMISTRY | BLOOD | 238277
+          50912, -- CREATININE | CHEMISTRY | BLOOD | 797476
+          50902, -- CHLORIDE | CHEMISTRY | BLOOD | 795568
+          50806, -- CHLORIDE, WHOLE BLOOD | BLOOD GAS | BLOOD | 48187
+          50931, -- GLUCOSE | CHEMISTRY | BLOOD | 748981
+          50809, -- GLUCOSE | BLOOD GAS | BLOOD | 196734
+          51221, -- HEMATOCRIT | HEMATOLOGY | BLOOD | 881846
+          50810, -- HEMATOCRIT, CALCULATED | BLOOD GAS | BLOOD | 89715
+          51222, -- HEMOGLOBIN | HEMATOLOGY | BLOOD | 752523
+          50811, -- HEMOGLOBIN | BLOOD GAS | BLOOD | 89712
+          50813, -- LACTATE | BLOOD GAS | BLOOD | 187124
+          51265, -- PLATELET COUNT | HEMATOLOGY | BLOOD | 778444
+          50971, -- POTASSIUM | CHEMISTRY | BLOOD | 845825
+          50822, -- POTASSIUM, WHOLE BLOOD | BLOOD GAS | BLOOD | 192946
+          51275, -- PTT | HEMATOLOGY | BLOOD | 474937
+          51237, -- INR(PT) | HEMATOLOGY | BLOOD | 471183
+          51274, -- PT | HEMATOLOGY | BLOOD | 469090
+          50983, -- SODIUM | CHEMISTRY | BLOOD | 808489
+          50824, -- SODIUM, WHOLE BLOOD | BLOOD GAS | BLOOD | 71503
+          51006, -- UREA NITROGEN | CHEMISTRY | BLOOD | 791925
+          51301, -- WHITE BLOOD CELLS | HEMATOLOGY | BLOOD | 753301
+          51300  -- WBC COUNT | HEMATOLOGY | BLOOD | 2371
       )
       and lab.valuenum is not null and lab.valuenum > 0 -- lab values cannot be 0 and cannot be negative
 )
@@ -193,17 +225,44 @@ with ce_stg as
 (
   select
     le_stg.icustay_id
-
-    , min(case when label = 'BILIRUBIN' then valuenum else null end) as BILIRUBIN_min
-    , max(case when label = 'BILIRUBIN' then valuenum else null end) as BILIRUBIN_max
-    , min(case when label = 'CREATININE' then valuenum else null end) as CREATININE_min
-    , max(case when label = 'CREATININE' then valuenum else null end) as CREATININE_max
-    , min(case when label = 'HEMOGLOBIN' then valuenum else null end) as HEMOGLOBIN_min
-    , max(case when label = 'HEMOGLOBIN' then valuenum else null end) as HEMOGLOBIN_max
-    , min(case when label = 'SODIUM' then valuenum else null end) as SODIUM_min
-    , max(case when label = 'SODIUM' then valuenum else null end) as SODIUM_max
-    , min(case when label = 'WBC' then valuenum else null end) as WBC_min
-    , max(case when label = 'WBC' then valuenum else null end) as WBC_max
+    , min(CASE WHEN label = 'ANION GAP' THEN valuenum ELSE null END) as ANIONGAP_min
+    , max(CASE WHEN label = 'ANION GAP' THEN valuenum ELSE null END) as ANIONGAP_max
+    , min(CASE WHEN label = 'ALBUMIN' THEN valuenum ELSE null END) as ALBUMIN_min
+    , max(CASE WHEN label = 'ALBUMIN' THEN valuenum ELSE null END) as ALBUMIN_max
+    , min(CASE WHEN label = 'BANDS' THEN valuenum ELSE null END) as BANDS_min
+    , max(CASE WHEN label = 'BANDS' THEN valuenum ELSE null END) as BANDS_max
+    , min(CASE WHEN label = 'BICARBONATE' THEN valuenum ELSE null END) as BICARBONATE_min
+    , max(CASE WHEN label = 'BICARBONATE' THEN valuenum ELSE null END) as BICARBONATE_max
+    , min(CASE WHEN label = 'BILIRUBIN' THEN valuenum ELSE null END) as BILIRUBIN_min
+    , max(CASE WHEN label = 'BILIRUBIN' THEN valuenum ELSE null END) as BILIRUBIN_max
+    , min(CASE WHEN label = 'CREATININE' THEN valuenum ELSE null END) as CREATININE_min
+    , max(CASE WHEN label = 'CREATININE' THEN valuenum ELSE null END) as CREATININE_max
+    , min(CASE WHEN label = 'CHLORIDE' THEN valuenum ELSE null END) as CHLORIDE_min
+    , max(CASE WHEN label = 'CHLORIDE' THEN valuenum ELSE null END) as CHLORIDE_max
+    , min(CASE WHEN label = 'GLUCOSE' THEN valuenum ELSE null END) as GLUCOSE_min
+    , max(CASE WHEN label = 'GLUCOSE' THEN valuenum ELSE null END) as GLUCOSE_max
+    , min(CASE WHEN label = 'HEMATOCRIT' THEN valuenum ELSE null END) as HEMATOCRIT_min
+    , max(CASE WHEN label = 'HEMATOCRIT' THEN valuenum ELSE null END) as HEMATOCRIT_max
+    , min(CASE WHEN label = 'HEMOGLOBIN' THEN valuenum ELSE null END) as HEMOGLOBIN_min
+    , max(CASE WHEN label = 'HEMOGLOBIN' THEN valuenum ELSE null END) as HEMOGLOBIN_max
+    , min(CASE WHEN label = 'LACTATE' THEN valuenum ELSE null END) as LACTATE_min
+    , max(CASE WHEN label = 'LACTATE' THEN valuenum ELSE null END) as LACTATE_max
+    , min(CASE WHEN label = 'PLATELET' THEN valuenum ELSE null END) as PLATELET_min
+    , max(CASE WHEN label = 'PLATELET' THEN valuenum ELSE null END) as PLATELET_max
+    , min(CASE WHEN label = 'POTASSIUM' THEN valuenum ELSE null END) as POTASSIUM_min
+    , max(CASE WHEN label = 'POTASSIUM' THEN valuenum ELSE null END) as POTASSIUM_max
+    , min(CASE WHEN label = 'PTT' THEN valuenum ELSE null END) as PTT_min
+    , max(CASE WHEN label = 'PTT' THEN valuenum ELSE null END) as PTT_max
+    , min(CASE WHEN label = 'INR' THEN valuenum ELSE null END) as INR_min
+    , max(CASE WHEN label = 'INR' THEN valuenum ELSE null END) as INR_max
+    , min(CASE WHEN label = 'PT' THEN valuenum ELSE null END) as PT_min
+    , max(CASE WHEN label = 'PT' THEN valuenum ELSE null END) as PT_max
+    , min(CASE WHEN label = 'SODIUM' THEN valuenum ELSE null END) as SODIUM_min
+    , max(CASE WHEN label = 'SODIUM' THEN valuenum ELSE null end) as SODIUM_max
+    , min(CASE WHEN label = 'BUN' THEN valuenum ELSE null end) as BUN_min
+    , max(CASE WHEN label = 'BUN' THEN valuenum ELSE null end) as BUN_max
+    , min(CASE WHEN label = 'WBC' THEN valuenum ELSE null end) as WBC_min
+    , max(CASE WHEN label = 'WBC' THEN valuenum ELSE null end) as WBC_max
 
   from le_stg
   group by le_stg.icustay_id
@@ -233,6 +292,7 @@ SELECT icu.subject_id, icu.hadm_id, icu.icustay_id, first_careunit
 , round((EXTRACT(EPOCH FROM (adm.dischtime-adm.admittime))/60/60/24) :: NUMERIC, 4) as hosp_los
 , EXTRACT('epoch' from icu.intime - pat.dob) / 60.0 / 60.0 / 24.0 / 365.242 as age_icu_in
 , pat.gender
+, pat.dod_hosp
 , RANK() OVER (PARTITION BY icu.subject_id ORDER BY icu.intime) AS icustay_id_order
 , hospital_expire_flag
 , CASE WHEN pat.dod IS NOT NULL 
@@ -245,7 +305,6 @@ INNER JOIN patients pat
 INNER JOIN admissions adm
 ON adm.hadm_id = icu.hadm_id    
 )
-
 -- Table #5: Exclusions
 , excl AS
 (
@@ -281,8 +340,6 @@ LEFT JOIN serv
 )
 
 SELECT ie.icustay_id, ie.subject_id, ie.hadm_id
--- , adm.HOSPITAL_EXPIRE_FLAG -- whether the patient died within the hospital
--- , EXTRACT('epoch' from ie.intime - pat.dob) / 60.0 / 60.0 / 24.0 / 365.242 as Age
 , HeartRate_Min, HeartRate_Mean, HeartRate_Max
 , DiasBP_Min, DiasBP_Max, DiasBP_Mean
 , SysBP_Min, SysBP_Max, SysBP_Mean
@@ -293,15 +350,31 @@ SELECT ie.icustay_id, ie.subject_id, ie.hadm_id
 
 , GCS_Min, GCS_Max
 
+-- lab values
+, ANIONGAP_min, ANIONGAP_max
+, ALBUMIN_min, ALBUMIN_max
+, BANDS_min, BANDS_max
+, BICARBONATE_min, BICARBONATE_max
 , BILIRUBIN_min, BILIRUBIN_max
 , CREATININE_min, CREATININE_max
+, CHLORIDE_min, CHLORIDE_max
+, GLUCOSE_min, GLUCOSE_max
+, HEMATOCRIT_min, HEMATOCRIT_max
 , HEMOGLOBIN_min, HEMOGLOBIN_max
+, LACTATE_min, LACTATE_max
+, PLATELET_min, PLATELET_max
+, POTASSIUM_min, POTASSIUM_max
+, PTT_min, PTT_max
+, INR_min, INR_max
+, PT_min, PT_max
 , SODIUM_min, SODIUM_max
+, BUN_min, BUN_max
 , WBC_min, WBC_max
 
 , co.age_icu_in, co.first_careunit, co.gender
 , co.hospital_expire_flag, co.icu_expire_flag
 , co.hosp_los, co.icu_los, co.icustay_id_order
+, co.dod_hosp
 
 FROM icustays ie
 inner join admissions adm
